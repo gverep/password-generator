a8c92f0b4b125fe496d8f0784a908a62a88282d6
Göksel Verep <142177502+gverep@users.noreply.github.com>
2025-02-08 18:48:24 +0300

    build: update package name and version format (#4)

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="password_generator",
-    version="0.1",
+    name="password-generator",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
